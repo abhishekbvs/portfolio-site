1d9779470c11cab76313bdbd211153d743188d89
Abhishek Bvs <bvsabhishek@gmail.com>
2019-12-13 02:57:52 +0530

    Add sass style sheets

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Footer = () => (
+   <footer>
+        © {new Date().getFullYear()}, Built with 💗  by Abhishek Bvs.
+   </footer>
+)
+
+export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,34 +1,18 @@
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+import Footer from './footer'
+import "../styles/index.sass"
 
 const Layout = ({ children }) => {
+
   return (
     <>
-      <Header siteTitle="Abhishek's Portfolio" />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+        <Header />
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with 💗  by Abhishek Bvs.
-       
-        </footer>
-      </div>
+        <Footer />
+
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
