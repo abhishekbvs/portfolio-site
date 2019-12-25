ac9133594a48e1f7411e8090facb66fa9de985fa
Abhishek Bvs <bvsabhishek@gmail.com>
2019-12-25 13:52:27 +0530

    Modify project name

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Abhishek Bvs`,
+        short_name: `Abhishek`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
