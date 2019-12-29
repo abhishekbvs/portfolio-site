8f56e831eeecbb1d2fc3fe7b006d12cb50252ebc
Abhishek Bvs <bvsabhishek@gmail.com>
2019-12-29 12:51:32 +0530

    Update time

--- src/components/face.js
+++ src/components/face.js
@@ -6,17 +6,17 @@ import Countdown from "../components/countdown";
 const Face = () => (
     <>
    <div id="face" className="container-fluid">
-        <h1><strong>Code Breaker Challenges</strong></h1>
+        <h1><strong>Code Breaker</strong></h1>
         <h2><strong>InCTF19</strong></h2>
    </div>
    <div id="challenge">
      <div className ="container">
-          <h2>Code breaker Challenge 1</h2>
+          <h2>Code breaker Challenge 4</h2>
           <p>  Break the code from the image  </p>
-          <img src={Challenge1} alt="Challenge 1" style={{ height: "50vh" }} />
+          <img src={Challenge1} alt="Challenge 4" style={{ height: "50vh" }} />
           <div  style={{ paddingTop : "1rem" }} >
                <h4><strong>Round ends in</strong></h4>
-               <Countdown deadline='December 29, 2019 13:00:00'/>
+               <Countdown deadline='December 29, 2019 13:30:00'/>
           </div>
           <br></br>
           <div>
--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => (
   
    <div id = "footer" >
-      <p> © Copyright {new Date().getFullYear()}, Built by <strong>Team InCTF19</strong>.</p>   
+      <p> © Copyright {new Date().getFullYear()}, Built by Team <strong> InCTF19</strong>.</p>   
    </div>  
   
 )
