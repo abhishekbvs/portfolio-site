06777b50689714195f244dc4c23c74ff7eed8d82
Abhishek Bvs <bvsabhishek@gmail.com>
2019-12-29 14:32:14 +0530

    Update Code

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,12 @@ module.exports = {
         icon: `src/images/my-icon.png`, // This path is relative to the root of the site.
       },
     },
-    
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-responsive-iframe`],
+      },
+    },    
     `gatsby-plugin-offline`,
     `gatsby-plugin-sass`
   ],
--- src/components/face.js
+++ src/components/face.js
@@ -14,6 +14,10 @@ const Face = () => (
           <h2>Code breaker Challenge 4</h2>
           <p>  Break the code from the image  </p>
           <img src={Challenge1} alt="Challenge 4" style={{ height: "50vh" }} />
+          <div style={{ paddingTop: "1rem" }}>
+               <h3><b>HINT</b></h3>
+               <p>This is a substitution cipher</p>
+          </div>
           <div  style={{ paddingTop : "1rem" }} >
                <h4><strong>Round ends in</strong></h4>
                <Countdown deadline='December 29, 2019 14:00:00'/>
@@ -23,6 +27,17 @@ const Face = () => (
                {/* <h4>The Winner is CADET 0x232</h4> */}
           </div>
           <br></br>
+     </div>
+   </div>
+   <div id="challenge">
+     <div className ="container">
+          <h2>Code breaker Challenge 5</h2>
+          <p>Complete all the challenges within the time limit and get the badge</p>
+          <iframe src="https://www.101computing.net/code-breaker/" width="1000" height="700">Challenge</iframe>
+          <div  style={{ paddingTop : "1rem" }} >
+               <h4><strong>Round ends in</strong></h4>
+               <Countdown deadline='December 29, 2019 15:00:00'/>
+          </div>
      </div>
    </div>
    </>
