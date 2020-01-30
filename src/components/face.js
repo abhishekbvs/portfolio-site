7f00e400b5d3a2ab61e1498563e9dd4fe175a2bd
Abhishek Bvs <bvsabhishek@gmail.com>
2020-01-30 06:55:11 +0530

    Type Racing

--- src/components/face.js
+++ src/components/face.js
@@ -6,38 +6,22 @@ import Countdown from "../components/countdown";
 const Face = () => (
     <>
    <div id="face" className="container-fluid">
-        <h1><strong>Code Breaker</strong></h1>
-        <h2><strong>InCTF19</strong></h2>
+        <h1><strong>Vidyut 2020</strong></h1>
+        <h2><strong>amFOSS</strong></h2>
    </div>
    <div id="challenge">
      <div className ="container">
-          <h2>Code breaker Challenge 4</h2>
-          <p>  Break the code from the image  </p>
-          <img src={Challenge1} alt="Challenge 4" style={{ height: "50vh" }} />
-          <div style={{ paddingTop: "1rem" }}>
-               {/* <h3><b>HINT</b></h3>
-               <p>This is a substitution cipher</p> */}
-          </div>
-          <div  style={{ paddingTop : "1rem" }} >
+          <h2>Type Racing</h2>
+          <p> The Stall is open. Intreseted to play and win the swag? Then dive in and start typing.  </p>
+          <iframe src="https://10fastfingers.com/widgets/ttembeddable/?dur=60" style={{width:'1200px',height:'450px'}}></iframe>
+          {/* <div  style={{ paddingTop : "1rem" }} >
                <h4><strong>Round ends in</strong></h4>
                <Countdown deadline='December 29, 2019 15:30:00'/>
-          </div>
+          </div> */}
           <br></br>
-          <div>
-               {/* <h4>The Winner is CADET 0x232</h4> */}
-          </div>
           <br></br>
-     </div>
-   </div>
-   <div id="challenge">
-     <div className ="container">
-          <h2>Code breaker Challenge 5</h2>
-          <p>Complete all the challenges within the time limit and get the badge</p>
-          <iframe src="https://www.101computing.net/code-breaker/?c=3" width="1000" height="700">Challenge</iframe>
-          <div  style={{ paddingTop : "1rem" }} >
-               <h4><strong>Round ends in</strong></h4>
-               <Countdown deadline='December 29, 2019 15:30:00'/>
-          </div>
+          <br></br>
+          <br></br>
      </div>
    </div>
    </>
