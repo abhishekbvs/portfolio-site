39db09bc0ae9aa1494d622b15a2dd05806bbbf8a
Abhishek Bvs <bvsabhishek@gmail.com>
2020-01-30 16:22:10 +0530

    Type racing

--- src/components/face.js
+++ src/components/face.js
@@ -1,7 +1,7 @@
 import React from "react";
-
-import Challenge1 from "../images/challenge.png";
-import Countdown from "../components/countdown";
+import Table from 'react-bootstrap/Table';
+// import Challenge1 from "../images/challenge.png";
+// import Countdown from "../components/countdown";
 
 const Face = () => (
     <>
@@ -13,9 +13,41 @@ const Face = () => (
      <div className ="container">
           <h2>Type Racing</h2>
           <p> The Stall is open. Intreseted to play and win the swag? Then dive in and start typing.  </p>
-          <iframe src="https://10fastfingers.com/widgets/ttembeddable/?dur=60" style={{width:'1200px',height:'450px'}}></iframe>
+          <iframe title="Type" src="https://10fastfingers.com/widgets/ttembeddable/?dur=60" style={{width:'1200px',height:'450px'}}></iframe>
           <br></br>
           <br></br>
+          <h2>Scoreboard</h2>
+          <Table striped bordered hover variant="dark" >
+               <thead>
+                    <tr>
+                         <th>Rank</th>
+                         <th>Name</th>
+                         <th>Speed in WPM</th>
+                         <th>Vidyut ID</th>
+                         <th>From</th>
+                    </tr>
+               </thead>
+                <tbody>
+                    <tr>
+                         <td>1</td>
+                         <td>Abhishek Bvs</td>
+                         <td>100</td>
+                         <td>VAM3128</td>
+                         <td>Amrita Vishwa Vidyapeetham</td>
+                    </tr>
+                         
+               </tbody>
+          </Table>  
+          <br></br>
+          <br></br>
+          <h2>Terms and Conditions</h2>
+          <p> By taking part in the competition you acknowledge and agree to abide by the following terms and conditions.
+          For any particular race an individual is eligible for only one prize.
+          The prizes are as stated and no cash or any other alternatives will be offered. Prizes are subject to availability and we reserve the right to substitute the prize with another of equal value without prior notice.
+          The organizers decision in all matters related with the contest, including the results, will be final and no negotiation in this regard will be entertained.
+          The terms and conditions are liable to change without prior notice according to the organizers discretion.</p>
+          <br></br>
+          
      </div>
    </div>
    </>
