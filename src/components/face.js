e74ec8e6a74faa6ec70601160d043ab37832b475
Abhishek Bvs <bvsabhishek@gmail.com>
2020-01-30 16:29:20 +0530

    Type racing

--- src/components/face.js
+++ src/components/face.js
@@ -13,7 +13,7 @@ const Face = () => (
      <div className ="container">
           <h2>Type Racing</h2>
           <p> The Stall is open. Intreseted to play and win the swag? Then dive in and start typing.  </p>
-          <iframe title="Type" src="https://10fastfingers.com/widgets/ttembeddable/?dur=60" style={{width:'1200px',height:'450px'}}></iframe>
+          <iframe title="Type" src="https://10fastfingers.com/widgets/ttembeddable/?dur=60" style={{width:'700px',height:'450px'}}></iframe>
           <br></br>
           <br></br>
           <h2>Scoreboard</h2>
